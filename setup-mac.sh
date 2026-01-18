9b0ba633a8b67f6ff583b2ae01aac34fe0100ad6
Daniel Oh <ohdoyoel@gmail.com>
2026-01-18 19:07:40 +0900

    feat: Update setup-mac.sh to install additional tools
    Added installation commands for various tools including wget, Docker, and several applications via Homebrew.

---
+++ setup-mac.sh
@@ -0,0 +1,27 @@
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# git
+git config --global user.name "ohdoyoel"
+git config --global user.email ohdoyoel@gmail.com
+
+# shell
+brew install wget
+
+# docker
+brew install docker
+
+# brew cask
+brew install --cask iterm2
+brew install --cask google-chrome
+brew install --cask slack
+brew install --cask notion
+brew install --cask rectangle
+brew install --cask topnotch
+
+# node
+brew install node
+brew install pnpm
